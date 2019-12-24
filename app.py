c40b0444855562efd34330291d575f924acb6885
j2personal <junjang17@gmail.com>
2019-12-24 17:25:44 -0600

    added new index

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def index():
     if request.method == 'GET':
         return render_template("index.html")
-    
+
 
 
 
