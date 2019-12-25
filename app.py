eabe04833f4ab7ea21a3908e16afa7a2d6e07d10
j2personal <junjang17@gmail.com>
2019-12-24 18:35:57 -0600

    adding db stuff

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, render_template
 import os
+import psycopg2
 
 app = Flask(__name__)
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("index.html")
+
+    if request.method == 'POST':
 
 
 
