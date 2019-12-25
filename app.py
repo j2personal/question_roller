e2ec485d5f2316811d4e0bce9b2c21d97f311a8c
j2personal <junjang17@gmail.com>
2019-12-24 21:06:19 -0600

    added view

--- app.py
+++ app.py
@@ -18,14 +18,39 @@ def index():
     if request.method == 'POST':
         cur = conn.cursor()
         cur.execute("SELECT question FROM questions")
-
         lst = cur.fetchall()
-        print(lst)
         cur.close()
 
         item = random.choice(lst)[0]
 
         return(item)
+
+@app.route('/view', methods=['GET'])
+def view():
+    if request.method == 'GET':
+        cur = conn.cursor()
+        cur.execute("SELECT question FROM questions")
+        lst = [item[0] for item in cur.fetchall()]
+
+        cur.close()
+        return render_template("view.html", lst=lst)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        request.form.get
+        cur = conn.cursor()
+        
+        cur.execute("SELECT question FROM questions")
+        lst = cur.fetchall()
+        cur.close()
+
+        item = random.choice(lst)[0]
+
+        return(item)
+
+
 
 
 
