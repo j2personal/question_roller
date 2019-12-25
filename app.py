ec5f650a426c9b8b0f9bf844f4a07730ed7f8978
j2personal <junjang17@gmail.com>
2019-12-24 20:20:00 -0600

    testing db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import os
 import psycopg2
+import random
 
 app = Flask(__name__)
 
@@ -11,18 +12,20 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
+        return render_template("index.html")
+
+
+    if request.method == 'POST':
         cur = conn.cursor()
         cur.execute("SELECT question FROM questions")
 
         lst = cur.fetchall()
         print(lst)
         cur.close()
-        return render_template("index.html")
 
+        item = random.choice(lst)[0]
 
-    if request.method == 'POST':
-
-        request.form.get("")
+        return(item)
 
 
 
